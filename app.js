f4c7c4849406abe85ea7c9244b69d0272c3536df
delta student <reter3246081@gmail.com>
2025-03-03 02:16:32 +0530

    first commit

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ let gameseq=[];
 let userseq=[];
 let btns=["yellow","red","purple","green"];  // array of classes 
 
+//1st step
 let started=false;
 let level=0;
-let h2=document.querySelector("h2");
-document.addEventListener("keypress",function(){
+document.addEventListener("keypress",()=>{
 if(started==false){
     console.log("game is started");
     started=true;
@@ -13,6 +13,8 @@ if(started==false){
 }    
 });
 
+
+//2nd-2
 function btnflash(btn){
     btn.classList.add("flash");
     setTimeout(()=>{
@@ -20,15 +22,19 @@ function btnflash(btn){
     },1000 );
 }
 
+//3rd-3
 function userflash(btn){
     btn.classList.add("userflash");
     setTimeout(()=>{
     btn.classList.remove("userflash");
     },1000);
 }
+let h2=document.querySelector("h2"); 
+//2nd step-1
 function levelup(){
-   userseq=[]; 
+   userseq=[];  
     level++;
+   
 h2.innerText=`level ${level}`;
 let randidx=Math.floor(Math.random()*4);
 let randclr=btns[randidx]; //will give classes e.g. rec yellow purple green
@@ -37,14 +43,13 @@ gameseq.push(randclr);
 btnflash(randbtn);
 
 }
-
+//4th-2
 function checkans(idx){
 //    console.log("current level :",level); 
-if(userseq[idx]==gameseq[idx]){
-    if(userseq.length==gameseq.length){
+if(userseq[idx]==gameseq[idx]){ //pehla idx dono ka same chahiye-base case
+    if(userseq.length==gameseq.length){ //saare sequence agar same hue
        setTimeout(levelup(),1000);
     }
-    // console.log("same value");
 }
 else{
     h2.innerHTML=`Game Over! your score was <b>${level}</b> <br> press any key to start.`;
@@ -52,25 +57,32 @@ else{
     document.querySelector("body").style.backgroundColor="red";
     setTimeout(()=>{
     document.querySelector("body").style.backgroundColor="white";
-    },150);
+    },1000); 
+   
+   // 5th step
     reset();
 }
 }
 
+//3rd-2
 function btnpress(){
    let btn =this; 
 //'this' will be the btn which is pressed
    userflash(btn);
    userclr=btn.getAttribute("id");
    userseq.push(userclr);
-   checkans(userseq.length-1);
+   //4th-1
+   checkans(userseq.length-1); //0th index
 
 }
+
+//3rd-1
 let allbtns=document.querySelectorAll(".btn");
 for(btn of allbtns){
     btn.addEventListener("click",btnpress);
 }
 
+//5th step-2
 function reset(){
     started=false;
     gameseq=[];
